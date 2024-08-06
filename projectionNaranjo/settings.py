b43968adfa043658a923577bf20eb62c94ef2ac3
lsoto <luisalexander.sotosegovia.3@gmail.com>
2024-08-06 03:50:01 -0500

    Subida 6

--- projectionNaranjo/settings.py
+++ projectionNaranjo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-a0)(mirnz+jmn$6dj3402i2f5p_3kb6zdg2f!uqteq7%xl9_m8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
+# Para abrir
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', 'projectionnaranjo-5rwq.onrender.com').split(',')]
 ALLOWED_HOSTS = []
 
